49e47ebd3a43770fff88dba817ffc05c5792a2a3
eguar1978 <maileguar@gmail.com>
2022-02-12 15:00:21 -0300

    Inicio de curso BackEnd

---
+++ Clase-01-12-02-2022/Clase-01-Desafio/index.js
@@ -0,0 +1,14 @@
+/**
+ * 
+*! Definir la función mostrarLista que reciba una lista de datos y muestre su contenido, si no está vacía,
+*! o de lo contrario muestre el mensaje: “lista vacía”. 
+*! Luego, invocarla con datos de prueba para verificar que funciona bien en ambos casos.
+
+*? Definir una función anónima que haga lo mismo que la del punto 1, e invocarla inmediatamente,
+*? pasando una lista con 3 números como argumento.
+
+*TODO Definir la función crearMultiplicador que reciba un número y devuelva una función anónima que reciba segundo número
+*TODO y dé como resultado el producto de ambos. Luego, a partir de la función definida, 
+*TODO crear dos funciones duplicar y triplicar, y probarlas con diferentes valores.
+* 
+*/
