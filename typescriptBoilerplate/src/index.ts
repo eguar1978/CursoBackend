db5842703d1a5534842ec602987b6a44224b2b22
eguar1978 <maileguar@gmail.com>
2022-02-12 15:03:30 -0300

    --

---
+++ typescriptBoilerplate/src/index.ts
@@ -0,0 +1,28 @@
+let let1:string = "let1";
+const num1:number = 1;
+const bool: boolean = false
+console.log(let1, num1, bool)
+const arrAny: any[] = [1, 'string', true, {name: "CoderHouse"}]
+const arrNum: number[] = [];
+arrNum.push(1);
+arrNum.push(2);
+console.log(arrAny, arrNum);
+
+type objType = {
+    nombre: string,
+    curso: string,
+    duracion: number,
+    arr: number[]
+}
+
+type obj2Type = {
+    obj1: objType;
+    arr: any[];
+}
+
+const obj: objType  = {
+    nombre: "",
+    curso: "",
+    duracion: 0,
+    arr: []
+}
