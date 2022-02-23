5a797e444f22c7503b22b48ef1ba6cb41f022f88
eguar1978 <maileguar@gmail.com>
2022-02-23 00:55:28 -0300

    los metodos funcionan ok

--- Clase-02/Desafio/desafio.js
+++ Clase-02/Desafio/desafio.js
@@ -1,12 +1,11 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 class Contenedor {
-
-    constructor(fileName) {
-        this.fileName = fileName;
-        this.objetos = [];
-    }
-/*
+  constructor(fileName) {
+    this.fileName = fileName;
+    this.objetos = [];
+  }
+  /*
   randomId() {
     let chars = "123456789";
     let string_length = 15;
@@ -20,42 +19,58 @@ class Contenedor {
 */
   async getAll() {
     try {
-      const productos = await fs.promises.readFile(this.fileName, "utf-8");
-      console.log(JSON.parse(productos));
+      const productos = await fs.readFile(this.fileName, "utf-8");
+      //console.log(JSON.parse(productos));
       return JSON.parse(productos);
     } catch (error) {
       console.log(error);
     }
   }
-/*
+
   async save(objeto) {
     try {
       this.objetos = await this.getAll();
-      this.objetos.push(objeto)
-      return JSON.stringify(this.objetos);
-
+      this.objetos.push(objeto);
+      return (this.objetos);
     } catch (error) {
       console.log(error);
     }
+  }
+
+  async getById(id){
+    try{
+      this.objetos = await this.getAll();
+      this.objetos = this.objetos.find(objetos => objetos.id == id);
+
+      if(this.objetos){
+        return (this.objetos);
+      }else{
+        return 'No existen registros para ese ID'
+      }
+      
+    }catch(error) {
+      console.log(error);
+    }
+  }
 
 }
-*/
-}
 
-const product = {
-  title: "Harina",
-  price: 97.75,
-  thumbnail:
-    "http://cdn.shopify.com/s/files/1/0437/9967/5045/products/1E7C5FA3-B08D-4A86-B954-549A1A04BB86_1200x1200.jpg",
-  id: 982319125158813
-};
-
-let productos = new Contenedor("products.json");
 (async () => {
-//console.log('1')
-console.log(await productos.getAll());
-})
-
+  const product = {
+    title: "Harina",
+    price: 97.75,
+    thumbnail:
+      "http://cdn.shopify.com/s/files/1/0437/9967/5045/products/1E7C5FA3-B08D-4A86-B954-549A1A04BB86_1200x1200.jpg",
+    id: 982319125158813,
+  };
+  let productos = new Contenedor("products.json");
+  console.log('Imprime el contenido del archivo');
+  console.log(await productos.getAll());
+  console.log('Imprime el contenido del archivo + el objeto que se le agrego');
+  console.log(await productos.save(product));
+  console.log('Buscar por ID');
+  console.log(await productos.getById(497497172573343));
+})();
 
 //console.log(productos.save(product));
 //console.log('2')
